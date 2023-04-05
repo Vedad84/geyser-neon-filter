41ef21ec783f77ce542c918c5fdf20f9eda77ea3
Denis Avvakumov <denisavvakumov@gmail.com>
2023-04-05 20:31:47 +0400

    NDEV-1528: Switch to label based stats

--- v2-advanced/clickhouse/configs/common/sql/prometheus_metrics.sql
+++ v2-advanced/clickhouse/configs/common/sql/prometheus_metrics.sql
@@ -5,60 +5,60 @@ CREATE OR REPLACE VIEW system.prometheus_metrics (
         `labels` Map(String, String),
         `type` String
     ) AS
-SELECT concat('events_', 'notify_block_local') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in notify_block_local' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'notify_block_local') AS labels,
     'gauge' AS type
 FROM events.notify_block_local
 UNION ALL
-SELECT concat('events_', 'notify_transaction_local') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in notify_transaction_local' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'notify_transaction_local') AS labels,
     'gauge' AS type
 FROM events.notify_transaction_local
 UNION ALL
-SELECT concat('events_', 'update_account_local') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in update_account_local' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'update_account_local') AS labels,
     'gauge' AS type
 FROM events.update_account_local
 UNION ALL
-SELECT concat('events_', 'update_slot_local') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in update_slot_local' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'update_slot_local') AS labels,
     'gauge' AS type
 FROM events.update_slot_local
 --- Distributed
 UNION ALL
-SELECT concat('events_', 'notify_block_distributed') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in notify_block_distributed' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'notify_block_distributed') AS labels,
     'gauge' AS type
 FROM events.notify_block_distributed
 UNION ALL
-SELECT concat('events_', 'notify_transaction_distributed') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in notify_transaction_distributed' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'notify_transaction_distributed') AS labels,
     'gauge' AS type
 FROM events.notify_transaction_distributed
 UNION ALL
-SELECT concat('events_', 'update_account_distributed') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in update_account_distributed' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'update_account_distributed') AS labels,
     'gauge' AS type
 FROM events.update_account_distributed
 UNION ALL
-SELECT concat('events_', 'update_slot_distributed') AS name,
+SELECT 'events_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
     'Number of rows in update_slot_distributed' AS help,
-    map('hostname', hostName()) AS labels,
+    map('hostname', hostName(), 'table', 'update_slot_distributed') AS labels,
     'gauge' AS type
 FROM events.update_slot_distributed
 ORDER BY name ASC;
