b10391e4c287df238d8af3cea8cc9a1e620a7816
Denis Avvakumov <denisavvakumov@gmail.com>
2023-04-07 13:49:15 +0400

    NDEV-1528: Add more metrics

--- v2-advanced/clickhouse/configs/common/sql/prometheus_metrics.sql
+++ v2-advanced/clickhouse/configs/common/sql/prometheus_metrics.sql
@@ -5,60 +5,151 @@ CREATE OR REPLACE VIEW system.prometheus_metrics (
         `labels` Map(String, String),
         `type` String
     ) AS
-SELECT 'events_row_count' AS name,
+SELECT 'local_event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in notify_block_local' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'notify_block_local') AS labels,
     'gauge' AS type
 FROM events.notify_block_local
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'local_event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in notify_transaction_local' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'notify_transaction_local') AS labels,
     'gauge' AS type
 FROM events.notify_transaction_local
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'local_event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in update_account_local' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'update_account_local') AS labels,
     'gauge' AS type
 FROM events.update_account_local
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'local_event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in update_slot_local' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'update_slot_local') AS labels,
     'gauge' AS type
 FROM events.update_slot_local
 --- Distributed
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in notify_block_distributed' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'notify_block_distributed') AS labels,
     'gauge' AS type
 FROM events.notify_block_distributed
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in notify_transaction_distributed' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'notify_transaction_distributed') AS labels,
     'gauge' AS type
 FROM events.notify_transaction_distributed
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in update_account_distributed' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'update_account_distributed') AS labels,
     'gauge' AS type
 FROM events.update_account_distributed
 UNION ALL
-SELECT 'events_row_count' AS name,
+SELECT 'event_row_count' AS name,
     toFloat64(COUNT(*)) AS value,
-    'Number of rows in update_slot_distributed' AS help,
+    'Number of rows in the table' AS help,
     map('hostname', hostName(), 'table', 'update_slot_distributed') AS labels,
     'gauge' AS type
 FROM events.update_slot_distributed
+UNION ALL
+--- Clickhouse stats
+SELECT 'clickhouse_select_query' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'counter' as type
+FROM system.events
+WHERE event = 'SelectQuery'
+---
+UNION ALL
+---
+SELECT 'clickhouse_failed_query' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'counter' as type
+FROM system.events
+WHERE event = 'FailedQuery'
+---
+UNION ALL
+---
+SELECT 'clickhouse_failed_select_query' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'counter' as type
+FROM system.events
+WHERE event = 'FailedSelectQuery'
+---
+UNION ALL
+---
+SELECT 'clickhouse_inserted_rows' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'counter' as type
+FROM system.events
+WHERE event = 'InsertedRows'
+---
+UNION ALL
+---
+SELECT 'clickhouse_kafka_messages_polled' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'counter' as type
+FROM system.events
+WHERE event = 'KafkaMessagesPolled'
+---
+UNION ALL
+---
+SELECT 'clickhouse_delayed_inserts' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'gauge' as type
+FROM system.metrics
+WHERE metric = 'DelayedInserts'
+---
+UNION ALL
+---
+SELECT 'clickhouse_query' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'gauge' as type
+FROM system.metrics
+WHERE metric = 'Query'
+---
+UNION ALL
+---
+SELECT 'clickhouse_merge' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'gauge' as type
+FROM system.metrics
+WHERE metric = 'Merge'
+---
+UNION ALL
+---
+SELECT 'clickhouse_move' as name,
+       toFloat64(value) AS value,
+       description as help,
+       map('hostname', hostName()) as labels,
+       'gauge' as type
+FROM system.metrics
+WHERE metric = 'Move'
 ORDER BY name ASC;
+
+
