55fb3f41f4bbb8fd6dd31701e6206a7190172ea8
Ivan Loboda <loboda.ivan.y@gmail.com>
2023-04-10 15:46:03 +0400

    revert ordering

--- v2-advanced/clickhouse/create_update_account.sql
+++ v2-advanced/clickhouse/create_update_account.sql
@@ -23,3 +23,36 @@ SETTINGS index_granularity=8192;
 
 CREATE TABLE IF NOT EXISTS events.update_account_distributed ON CLUSTER '{cluster}' AS events.update_account_local
 ENGINE = Distributed('{cluster}', events, update_account_local, xxHash64(CONCAT(toString(slot), arrayStringConcat(txn_signature,''))));
+
+CREATE TABLE IF NOT EXISTS events.update_account_queue ON CLUSTER '{cluster}' (
+    pubkey Array(UInt8),
+    lamports UInt64,
+    owner Array(UInt8),
+    executable Bool,
+    rent_epoch UInt64,
+    data Array(UInt8),
+    write_version UInt64,
+    txn_signature Array(Nullable(UInt8)),
+    slot UInt64,
+    is_startup Bool,
+    retrieved_time DateTime64 DEFAULT now64()
+)   ENGINE = Kafka SETTINGS
+    kafka_broker_list = 'kafka:29092',
+    kafka_topic_list = 'update_account',
+    kafka_group_name = 'clickhouse',
+    kafka_num_consumers = 1,
+    kafka_format = 'JSONEachRow';
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS events.update_account_queue_mv ON CLUSTER '{cluster}' to events.update_account_distributed
+AS  SELECT pubkey,
+           lamports,
+           owner,
+           executable,
+           rent_epoch,
+           data,
+           write_version,
+           txn_signature,
+           slot,
+           is_startup,
+           retrieved_time
+FROM events.update_account_queue;
